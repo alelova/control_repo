87d2ff4857041d0a0a7d8aee2e3dbd5c03bcaf4e
Alejandro <alelova@gmail.com>
2022-03-04 16:11:56 +0100

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,5 +1,5 @@
 class profile::minecraft{
-  class ('minecraft':
+  class {'minecraft':
     url='https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar'
-  )
+  }
 }
